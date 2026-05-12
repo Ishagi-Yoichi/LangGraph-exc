bd00786a796848c31493ea5d7d6264095aa301cb
Ishagi-Yoichi <nikunjkr1752003@gmail.com>
2026-05-12 17:08:46 +0530

    agent tool calling

---
+++ src/lg_exc/stage_01/agent_with_tool_call.py
@@ -0,0 +1,14 @@
+from langgraph.graph import END, START, MessagesState, StateGraph
+
+def mock_llm(state: MessagesState) -> MessagesState:
+    return {"messages": [{"role": "ai", "content": "hello world"}]}
+
+graph = StateGraph(MessagesState)
+graph.add_node("llm",mock_llm)
+graph.add_edge(START, "mock_llm")
+graph.add_edge("mock_llm", END)
+
+graph = graph.compile()
+
+result = graph.invoke({"messages": [{"role": "user", "content": "What is the weather in Tokyo?"}]})
+print(result)
